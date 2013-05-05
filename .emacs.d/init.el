74a842f9519269047304ab026802132dbfd9b3a2
Julian Berman <Julian@GrayVines.com>
2013-05-05 01:38:00 -0400

    OK OK OK

---
+++ .emacs.d/init.el
@@ -0,0 +1,14 @@
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+(require 'evil)
+(evil-mode 1)
