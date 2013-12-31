7079cd371c7b08dcfae7741fc278c52e3f0caeee
Julian Berman <Julian@GrayVines.com>
2013-12-31 18:56:24 -0500

    Bump

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,11 @@
 set -e
 
 DOTFILES_URL='https://github.com/Julian/dotfiles.git'
-DOTFILES_DEST=~/.dotfiles
-DEVELOPMENT=~/Development
+DOTFILES_DEST=$HOME/.dotfiles
 
 
 if [ -z "$DOTFILES_DEBUG" ]; then
-    git_verbosity='--quiet'
+    verbosity='--quiet'
 fi
 
 
@@ -29,10 +28,10 @@ clone_dotfiles()
 
         ensure_installed git
         (
-            git clone $git_verbosity $DOTFILES_URL $DOTFILES_DEST
+            git clone $verbosity $DOTFILES_URL $DOTFILES_DEST
             cd $DOTFILES_DEST
-            git submodule $git_verbosity init
-            git submodule $git_verbosity update
+            git submodule $verbosity init
+            git submodule $verbosity update
         )
     else
         echo "Existing dotfiles found in $DOTFILES_DEST."
@@ -51,7 +50,7 @@ ensure_has_homebrew()
     printf 'Checking for homebrew... '
      bin_exists brew && printf 'found\n' || {
         printf 'installing\n'
-        ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+        ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
     }
 }
 
@@ -63,7 +62,7 @@ bin_exists()
 check_installed()
 {
     if bin_exists brew; then
-        brew list | grep "\<$1\>" >/dev/null
+        brew list | grep "\b$1\b" >/dev/null
     elif bin_exists dpkg-query; then
         [ "`dpkg-query -W -f='${Status}\n' $1`" = 'install ok installed' ]
     fi
@@ -79,20 +78,11 @@ ensure_installed()
 install()
 {
     printf "Installing $@..."
-    sudo apt-get install --quiet --yes --force-yes $@
+    sudo apt-get install $verbosity --yes --force-yes $@
     printf 'done\n'
 }
 
 continue_with_dot()
 {
-    exec ~/.dotfiles/dot install
+    exec $DOTFILES_DEST/dot install
 }
-
-ensure_python_installed()
-{
-    for package; do
-        check_installed "python-$package" || install "python-$package"
-    done
-}
-
-main
