b1b43edc9c8d801adc2fd836e3abc071fc173c0d
Julian Berman <Julian@GrayVines.com>
2015-09-21 17:24:18 -0400

    Newer emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,23 +1,27 @@
 ; === Basic ===
 (global-hl-line-mode 1)
 
-; === el-get ===
-
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
-
 ; === Plugins ===
 
-(require 'color-theme-solarized)
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+    'package-archives
+    '("melpa" . "http://melpa.org/packages/")
+    t)
+  (package-initialize))
+
+(if (not (package-installed-p 'use-package))
+  (progn
+    (package-refresh-contents)
+    (package-install 'use-package)))
+
+(require 'use-package)
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
+
+(use-package evil)
 
 (require 'evil)
 (evil-mode 1)
