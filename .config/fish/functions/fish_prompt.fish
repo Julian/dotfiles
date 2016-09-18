e8547e97f3b3cd24dda8ca60fdb4ae3c7ef2ea57
Julian Berman <Julian@GrayVines.com>
2016-09-18 12:37:36 -0400

    More

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,19 @@
+function fish_prompt
+    set -l color_cwd
+    set -l suffix
+
+    switch $USER
+    case root toor
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+            else
+                set color_cwd $fish_color_cwd
+            end
+            set suffix '#'
+    case '*'
+        set color_cwd $fish_color_cwd
+        set suffix '>'
+    end
+
+    echo -n -s (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+end
