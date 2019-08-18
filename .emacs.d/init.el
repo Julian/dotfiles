b80746311bbf66bff3bedf20c4b38d1901d51322
Julian Berman <Julian@GrayVines.com>
2019-08-18 17:43:44 +0200

    HTTPS

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,7 +7,7 @@
   (require 'package)
   (add-to-list
     'package-archives
-    '("melpa" . "http://melpa.org/packages/")
+    '("melpa" . "https://melpa.org/packages/")
     t)
   (package-initialize))
 
