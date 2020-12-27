0a95de12b39578111bd39737419a744c3a1b77d0
Julian Berman <Julian@GrayVines.com>
2020-12-27 15:36:47 -0500

    Telescope config (yet unused except locally).

---
+++ .config/nvim/lua/config/telescope.lua
@@ -0,0 +1,27 @@
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+local builtin = require('telescope.builtin')
+
+function find_symbols()
+  if vim.tbl_isempty(vim.fn.tagfiles()) then
+    builtin.lsp_document_symbols()
+  else
+    builtin.tags()
+  end
+end
+
+function find_symbols_from_buffer()
+  builtin.current_buffer_tags()
+end
+
+telescope.setup{
+  defaults = {
+    prompt_position = "top",
+    sorting_strategy = "ascending",
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close
+      },
+    },
+  }
+}
