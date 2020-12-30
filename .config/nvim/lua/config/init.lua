06e04cadbedfd54caaddd3841d417449581c1a39
Julian Berman <Julian@GrayVines.com>
2020-12-30 12:33:00 -0500

    Treesitter config.

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,18 +1,1 @@
-require('nvim-treesitter.configs').setup{
-  ensure_installed = "maintained",
-  highlight = { enable = true },
-  indent = { enable = false },
-  textobjects = {
-    select = {
-      enable = true,
-      keymaps = {
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-        ["aa"] = "@parameter.outer",
-        ["ia"] = "@parameter.inner",
-      },
-    },
-  }
-}
+require('config.treesitter')
