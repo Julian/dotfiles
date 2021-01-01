414025eb9fccac82289736d06d142e7ad4b69f0f
Julian Berman <Julian@GrayVines.com>
2021-01-01 10:04:19 -0500

    And add the first version of LSP configurin'.

---
+++ .config/nvim/lua/config/lsp.lua
@@ -0,0 +1,56 @@
+local completion = require('completion')
+local lspconfig = require('lspconfig')
+
+function cmd(mode, key, cmd)
+  vim.api.nvim_buf_set_keymap(
+    0, mode, key, '<cmd>lua ' .. cmd .. '<CR>', {noremap = true}
+  )
+end
+
+function maybe_hover()
+  if vim.tbl_isempty(vim.lsp.diagnostic.get_line_diagnostics()) then
+    vim.lsp.buf.hover()
+  else
+    vim.lsp.diagnostic.show_line_diagnostics()
+  end
+end
+
+local attached = function(client)
+  cmd('n', 'K', 'maybe_hover()')
+  cmd('n', 'gd', 'vim.lsp.buf.definition()')
+  cmd('n', 'gi', 'vim.lsp.buf.implementation()')
+  cmd('n', 'gr', 'vim.lsp.buf.references()')
+  cmd('n', 'gy', 'vim.lsp.buf.type_definition()')
+  cmd('n', '<C-]>', 'vim.lsp.buf.definition()')
+  vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  completion.on_attach(client)
+end
+
+local opts = {on_attach = attached}
+local lsps = {
+  pyls = {},
+  sumneko_lua = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+    },
+  },
+  tsserver = {},
+  vimls = {},
+  leanls = {},
+}
+
+for lsp, lsp_opts in pairs(lsps) do
+  lspconfig[lsp].setup(vim.tbl_extend("force", opts, lsp_opts))
+end
