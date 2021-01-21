83485342cb83fd9a53b0e3367317f3084f507ebf
Julian Berman <Julian@GrayVines.com>
2021-01-20 23:03:47 -0500

    Run completion and LSP configs.

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,4 +1,7 @@
+require('config.completion')
+require('config.lsp')
 require('config.treesitter')
+
 require('lean').setup{
   lsp = {
     cmd = {"lean-language-server", "--stdio", "--", "-M", "4096"},
