cb9f8f607615efc035fed858fd71a4e5909bdb19
Julian Berman <Julian@GrayVines.com>
2021-11-10 08:25:15 -0500

    Dead code for now.

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -2,18 +2,6 @@ local telescope = require('telescope')
 local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
 
-function find_symbols()
-  if vim.tbl_isempty(vim.fn.tagfiles()) then
-    builtin.lsp_document_symbols()
-  else
-    builtin.tags()
-  end
-end
-
-function find_symbols_from_buffer()
-  builtin.current_buffer_tags()
-end
-
 telescope.setup{
   defaults = {
     layout_config = {
