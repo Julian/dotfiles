4891f3c47f8b71be1959b7c63d70ee67225f51d4
Julian Berman <Julian@GrayVines.com>
2022-04-11 17:49:20 -0400

    Make a big list of treesitter languages...
    See nvim-treesitter/nvim-treesitter@acd01551a3185118f58f0708b7fffdb2465e39f1

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -1,14 +1,21 @@
 if vim.g.loaded_nvim_treesitter ~= 1 then return end
 
 require('nvim-treesitter.configs').setup{
-  ensure_installed = SMALL_FILESYSTEM and { "python" } or "maintained",
+  ensure_installed = {
+    'bash', 'c', 'c_sharp', 'clojure', 'cmake', 'comment', 'cpp', 'css',
+    'cuda', 'dockerfile', 'dot', 'haskell', 'html', 'java', 'javascript',
+    'json', 'latex', 'llvm', 'lua', 'make', 'markdown', 'ninja', 'nix',
+    'python', 'query', 'regex', 'rst', 'ruby', 'rust', 'tlaplus', 'toml',
+    'typescript', 'vim', 'yaml', 'zig',
+  },
+  ignore_install = { 'phpdoc' },
   highlight = {
     enable = true,
-    disable = { "rst" },
+    disable = { 'rst' },
   },
   indent = {
     enable = true,
-    disable = { "python" },
+    disable = { 'python' },
   },
   playground = {
     enable = true,
@@ -18,19 +25,19 @@ require('nvim-treesitter.configs').setup{
   query_linter = {
     enable = true,
     use_virtual_text = true,
-    lint_events = { "BufWrite", "CursorHold" },
+    lint_events = { 'BufWrite', 'CursorHold' },
   },
   endwise = { enable = true },
   textobjects = {
     select = {
       enable = true,
       keymaps = {
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-        ["aa"] = "@parameter.outer",
-        ["ia"] = "@parameter.inner",
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+        ['aa'] = '@parameter.outer',
+        ['ia'] = '@parameter.inner',
       },
     },
   }
