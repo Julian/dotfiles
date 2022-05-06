6481c3afd61504aaedbbfe1da12209f410700c44
Julian Berman <Julian@GrayVines.com>
2022-05-06 10:28:01 -0400

    peek definition keybind.

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -1,6 +1,23 @@
 local lspconfig = require('lspconfig')
 local notify = require('notify')
 
+local function preview_location_callback(_, result, _)
+  if result == nil or vim.tbl_isempty(result) then
+    vim.notify('No definition found.')
+    return nil
+  end
+  if vim.tbl_islist(result) then
+    vim.lsp.util.preview_location(result[1])
+  else
+    vim.lsp.util.preview_location(result)
+  end
+end
+
+local function peek_definition()
+  local params = vim.lsp.util.make_position_params()
+  return vim.lsp.buf_request(0, 'textDocument/definition', params, preview_location_callback)
+end
+
 local function on_attach(client, bufnr)
   local function cmd(mode, lhs, rhs)
     vim.keymap.set(mode, lhs, rhs, { noremap = true, buffer = true })
@@ -10,6 +27,7 @@ local function on_attach(client, bufnr)
 
   cmd('n', 'gd', vim.lsp.buf.definition)
   cmd('n', 'gD', vim.lsp.buf.declaration)
+  cmd('n', 'gK', peek_definition)
   cmd('n', '<leader>Li', vim.lsp.buf.implementation)
   cmd('n', '<leader>Lr', vim.lsp.buf.references)
 
