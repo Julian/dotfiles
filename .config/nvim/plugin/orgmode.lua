c02b6dfa1b70ddc0ba0f83b7ca44db900aec52e7
Julian Berman <Julian@GrayVines.com>
2022-06-27 14:16:14 -0400

    Put orgmode on localleader now that nvim-orgmode/orgmode#329 is merged.

--- .config/nvim/plugin/orgmode.lua
+++ .config/nvim/plugin/orgmode.lua
@@ -2,6 +2,7 @@ local orgmode = require('orgmode')
 local ORG_HOME = vim.fn.expand('$XDG_DATA_HOME/org')
 orgmode.setup_ts_grammar()
 orgmode.setup{
+  mappings = { prefix = '<LocalLeader>' },
   org_agenda_files = { ORG_HOME .. '/**' },
   org_default_notes_file = ORG_HOME .. '/refile.org',
 }
