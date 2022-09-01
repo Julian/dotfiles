59010b3304aa1a10fd0b5f59660f6741722a03e2
Julian Berman <Julian@GrayVines.com>
2022-09-01 10:10:31 +0300

    Don't show full-screen overlays please.

--- .config/nvim/plugin/init.lua
+++ .config/nvim/plugin/init.lua
@@ -1,5 +1,9 @@
 vim.diagnostic.config{ severity_sort = true, float = { border = 'rounded' } }
+
 vim.notify = require('notify')
+vim.notify.setup{
+  max_width = 100,
+}
 
 local diagnostics_active = true
 vim.keymap.set('n', '<leader>ttd', function()
