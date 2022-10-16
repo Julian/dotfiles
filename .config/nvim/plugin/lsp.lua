e205da3e646fe4af0bf85e449898bccc9c2024bb
Julian Berman <Julian@GrayVines.com>
2022-10-16 15:00:40 -0400

    Deprecated cmp-nvim-lsp thing apparently.

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -94,9 +94,7 @@ table.insert(runtime_path, "lua/?/init.lua")
 
 local opts = {
   on_attach = on_attach,
-  capabilities = require'cmp_nvim_lsp'.update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-  )
+  capabilities = require'cmp_nvim_lsp'.default_capabilities()
 }
 local lsps = {
   clangd = {},
