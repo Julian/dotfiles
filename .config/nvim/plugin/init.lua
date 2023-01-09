d9ae5a431e38072664a9bb74ffd908ad828d5c5c
Julian Berman <Julian@GrayVines.com>
2023-01-09 18:11:28 -0500

    Sigh, disable, not hide, which just temporarily hides.

--- .config/nvim/plugin/init.lua
+++ .config/nvim/plugin/init.lua
@@ -7,9 +7,9 @@ vim.notify.setup{
 
 vim.keymap.set('n', '<leader>ttd', function()
   if vim.b.diagnostics_hidden then
-    vim.diagnostic.show(nil, 0)
+    vim.diagnostic.enable(0)
   else
-    vim.diagnostic.hide(nil, 0)
+    vim.diagnostic.disable(0)
   end
   vim.b.diagnostics_hidden = not vim.b.diagnostics_hidden
 end)
