938204db01cb6c5706c9b3ccde78c4cb03a190c1
Julian Berman <Julian@GrayVines.com>
2023-07-02 12:04:32 +0200

    Tell cmp to use bordered windows.

--- .config/nvim/plugin/completion.lua
+++ .config/nvim/plugin/completion.lua
@@ -13,6 +13,10 @@ end
 
 cmp.setup{
   snippet = { expand = function(args) vim.fn["vsnip#anonymous"](args.body) end },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
   mapping = {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
