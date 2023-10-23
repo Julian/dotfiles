a9a72bb56c34fdc8b8968eff20e7fcf14ffc3dae
Julian Berman <Julian@GrayVines.com>
2023-10-22 20:56:00 -0400

    assert.is.truthy should swap too.

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -2,7 +2,7 @@ vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 
 vim.b.switch_definitions = {
-    { 'is_true', 'is_false' },
-    { 'is_truthy', 'is_falsy' },
-    vim.fn['switch#Words']({ 'pending', 'it' })
+  { 'is_true', 'is_false' },
+  { 'truthy', 'falsy' },
+  vim.fn['switch#Words']({ 'pending', 'it' })
 }
