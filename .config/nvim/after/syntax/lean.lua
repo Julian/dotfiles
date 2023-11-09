44e383c8814227e366a34292154f204d7ea268b0
Julian Berman <Julian@GrayVines.com>
2023-11-09 16:57:10 -0500

    Replace the old semantic token names with the ones from the merged PR.

--- .config/nvim/after/syntax/lean.lua
+++ .config/nvim/after/syntax/lean.lua
@@ -1,27 +1,6 @@
 -- See https://github.com/theHamsta/nvim-semantic-tokens/blob/master/doc/nvim-semantic-tokens.txt
 local mappings = {
-  LspKeyword = "@keyword",
-  LspVariable = "@variable",
-  LspNamespace = "@namespace",
-  LspType = "@type",
-  LspClass = "@type.builtin",
-  LspEnum = "@constant",
-  LspInterface = "@type.definition",
-  LspStruct = "@structure",
-  LspTypeParameter = "@type.definition",
-  LspParameter = "@parameter",
-  LspProperty = "@property",
-  LspEnumMember = "@field",
-  LspEvent = "@variable",
-  LspFunction = "@function",
-  LspMethod = "@method",
-  LspMacro = "@macro",
-  LspModifier = "@keyword.function",
-  LspComment = "@comment",
-  LspString = "@string",
-  LspNumber = "@number",
-  LspRegexp = "@string.special",
-  LspOperator = "@operator",
+  ['@lsp.type.variable'] = 'Identifier'
 }
 
 for from, to in pairs(mappings) do
