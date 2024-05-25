bf0e18e42fcc4d237972425df5719ddbf7acb34e
Julian Berman <Julian@GrayVines.com>
2024-05-25 21:06:59 +0300

    Another rst heading thing

--- .config/nvim/after/ftplugin/rst.lua
+++ .config/nvim/after/ftplugin/rst.lua
@@ -5,5 +5,6 @@ vim.opt_local.formatoptions:remove('c')
 
 vim.keymap.set({'n'}, '<localleader>_', 'yypVr-', { buffer = true })
 vim.keymap.set({'n'}, '<localleader>+', 'yypVr=', { buffer = true })
+vim.keymap.set({'n'}, '<localleader>6', 'yypVr^', { buffer = true })
 vim.keymap.set({'n'}, '<localleader>-', 'yyPVr-yyjp', { buffer = true })
 vim.keymap.set({'n'}, '<localleader>=', 'yyPVr=yyjp', { buffer = true })
