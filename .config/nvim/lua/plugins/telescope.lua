0cdff3b4150e3146ce521b42b883d6664f38ee82
Julian Berman <Julian@GrayVines.com>
2024-07-07 12:12:38 -0400

    Possibly this is needed for :Telescope notify.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -16,6 +16,7 @@ return {
     config = function(_, opts)
       local telescope = require('telescope')
       telescope.setup(opts)
+      telescope.load_extension('notify')
       telescope.load_extension('ui-select')
     end,
     opts = function(_)
