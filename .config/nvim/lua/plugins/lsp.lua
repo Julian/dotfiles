c7c51bb848341115328aeabca19a561e75393fb1
Julian Berman <Julian@GrayVines.com>
2024-07-14 20:17:26 -0400

    Uh. Whoops, don't quash gi please.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -21,7 +21,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gb', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', 'gK', peek_definition, opts)
 
     if client.supports_method('textDocument/rename') then
