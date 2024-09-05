8067595561e7b81574256ca63c778eebf5724be2
Julian Berman <Julian@GrayVines.com>
2024-09-05 12:13:33 -0400

    Move rocks out of $XDG_DATA_HOME on macOS.
    Like venvs, this means they end up with spaces in their shebangs...

--- .config/luarocks.lua
+++ .config/luarocks.lua
@@ -1,6 +1,8 @@
+-- Avoid the usual shebang-with-spaces-makes-not-nice issue.
+local data_home = platforms.macos and (home .. '/.local/share') or os_getenv("XDG_DATA_HOME")
 rocks_trees = {
   {
     name = 'user',
-    root = os_getenv("XDG_DATA_HOME") .. '/luarocks'
+    root = data_home .. '/luarocks'
   },
 }
