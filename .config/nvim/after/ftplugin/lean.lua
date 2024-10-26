0864187d6acb16d71125ba1e8baa38216df91a63
Julian Berman <Julian@GrayVines.com>
2024-10-26 17:04:16 -0400

    Only live grep lean files in the lean live grep.

--- .config/nvim/after/ftplugin/lean.lua
+++ .config/nvim/after/ftplugin/lean.lua
@@ -5,6 +5,7 @@ vim.bo.textwidth = 100
 
 vim.keymap.set('n', '<LocalLeader>g', function()
     require'telescope.builtin'.live_grep{
+      glob_pattern = '*.lean',
       path_display = { 'tail' },
       search_dirs = require('lean').current_search_paths()
     }
