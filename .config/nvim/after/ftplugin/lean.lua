af6d8969f827954ab9356ca43e0b5964f3b53fda
Julian Berman <Julian@GrayVines.com>
2024-11-23 12:09:06 -0500

    RIP Lean 3.

--- .config/nvim/after/ftplugin/lean.lua
+++ .config/nvim/after/ftplugin/lean.lua
@@ -4,14 +4,12 @@ vim.wo.signcolumn = 'yes'
 vim.bo.textwidth = 100
 
 vim.keymap.set('n', '<LocalLeader>g', function()
-    require'telescope.builtin'.live_grep{
-      glob_pattern = '*.lean',
-      path_display = { 'tail' },
-      search_dirs = require('lean').current_search_paths()
-    }
+  require'telescope.builtin'.live_grep{
+    glob_pattern = '*.lean',
+    path_display = { 'tail' },
+    search_dirs = require('lean').current_search_paths()
+  }
 end, { buffer = true, desc = 'live grep the Lean search path.' })
 vim.keymap.set('n', '<LocalLeader>l', function()
-    require'telescope'.extensions.loogle.loogle{}
+  require'telescope'.extensions.loogle.loogle{}
 end, { buffer = true, desc = 'search Loogle via Telescope' })
-
-vim.cmd.highlight('link leanTactic Green')
