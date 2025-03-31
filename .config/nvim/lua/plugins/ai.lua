da86a2be09d02db9f4cb08ca3482ad1c18cf4eb0
Julian Berman <Julian@GrayVines.com>
2025-03-31 09:41:42 -0400

    Default to sonnet 3.7 thinking.

---
+++ .config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      server_opts_overrides = {
+        settings = {
+          telemetry = { telemetryLevel = 'off' },
+        },
+      },
+    },
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' },
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken',
+    opts = {
+      model = 'claude-3.7-sonnet-thought',
+      mappings = {
+        complete = { normal = '', insert = '' },
+        close = { normal = '', insert = '' },
+        reset = { normal = '<localleader>r' },
+        submit_prompt = {},
+        toggle_sticky = {},
+        clear_stickies = {},
+        accept_diff = {},
+        jump_to_diff = { normal = ']c', insert = false },
+        quickfix_answers = {},
+        quickfix_diffs = {},
+        yank_diff = {},
+        show_diff = {},
+        show_info = {},
+        show_context = {},
+        show_help = { normal = '<localleader>/' },
+      }
+    },
+    keys = {
+      {
+        '<leader>c',
+        function() require('CopilotChat').toggle() end,
+        desc = 'Toggle Copilot Chat',
+      },
+    },
+    cmd = {
+      'CopilotChat',
+      'CopilotChatOpen',
+      'CopilotChatClose',
+      'CopilotChatToggle',
+      'CopilotChatPrompts',
+      'CopilotChatModels',
+      'CopilotChatAgents',
+    },
+  },
+}
