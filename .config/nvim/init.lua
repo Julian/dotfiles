2ca77c2fedfac22cea0254cf6775af3ed5ed71bd
Julian Berman <Julian@GrayVines.com>
2025-04-11 09:47:33 -0400

    Add back longest to completeopt.
    It's not supported by blink, but when I get some time, I'm going to stop using blink...

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -57,7 +57,7 @@ vim.opt.tags:append('.git/tags;$HOME')  -- look upward until $HOME for tags
 
 -- Completion --
 
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect', 'fuzzy' }
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect', 'fuzzy', 'longest', 'popup' }
 
 vim.o.pumblend = 5                    -- slightly transparent pum window
 vim.opt.suffixes:append{ '.backup', '.hbm', '.ini' }  -- lower priority
