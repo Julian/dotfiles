f857a10869d147056b5aff348552f099e6333e51
Julian Berman <Julian@GrayVines.com>
2025-11-15 11:26:46 +0100

    First beancount switch.

--- .config/nvim/after/ftplugin/beancount.lua
+++ .config/nvim/after/ftplugin/beancount.lua
@@ -5,4 +5,13 @@ vim.bo.textwidth = 120
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 
-vim.keymap.set('n', '<localleader>t', 'oExpenses:Taxes:Sales                                       USD', { buffer = true })
+vim.b.switch_definitions = {
+  vim.g.switch_builtins.true_false,
+  { 'open', 'close' },
+}
+
+vim.keymap.set(
+  'n', '<localleader>t',
+  'oExpenses:Taxes:Sales                                       USD',
+  { buffer = true, desc = 'Add a sales tax line.' }
+)
